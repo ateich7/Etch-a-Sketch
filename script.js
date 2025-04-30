a17655facde15a17a6b31904b905a5901668ab61
ateich7 <140982241+ateich7@users.noreply.github.com>
2025-04-30 16:34:01 -0400

    Create grid size adjusting button
    allows user to adjust the dimsensions of the grid

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("bal");
 
 const container = document.getElementById("cont")
 
-for (let i = 0 ; i < 256 ; i++){
+for (let i = 0; i < 256; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
     container.appendChild(square);
@@ -12,5 +12,28 @@ for (let i = 0 ; i < 256 ; i++){
     });
 }
 
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+    let size = prompt("Enter size:")
 
+    if (size && size < 100 && size > 0) {
+        container.innerHTML = "";
+        let measurment = (960 / size);
+        for (let i = 0; i < (size*size); i++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.style.height = measurment + "px";
+            square.style.width = measurment + "px";
+            container.appendChild(square);
+
+            square.addEventListener("mouseover", () => {
+                square.style.backgroundColor = "#DDDDDD"
+            });
+        }
+    } else {
+        alert("Please enter a number below 100")
+    }
+    // then create a new grid with the new size setting
+// 960 x 960 = total area, then 
+})
 
